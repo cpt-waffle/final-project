6814d4dfde155bd9a52696d9faf6e0e872cd9947
vasiliy-klimkin <vasily.klimkin@gmail.com>
2022-10-27 12:01:31 -0400

    display cats on page

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,19 @@ import axios from 'axios';
 import {useState, useEffect} from 'react';
 
 function App() {
+  const [cats, setCats] = useState([]);
+
   useEffect(() => {
     axios.get('/cats').then(res => {
       console.log(res.data);
+      setCats(res.data);
     })
   }, [])
 
   return (
     <div className="App">
       <h1>Cats App!</h1>
-      
+      {cats.length ? cats.map(cat => <li>{cat}</li>) : <h2>Loading :|</h2>}
     </div>
   );
 }

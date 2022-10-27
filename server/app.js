b940f3d212771e48ff7f424f00ea281cc8210c02
vasiliy-klimkin <vasily.klimkin@gmail.com>
2022-10-27 12:18:38 -0400

    add routes

--- server/app.js
+++ server/app.js
@@ -20,4 +20,17 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cats', catsRouter);
 
+app.get('/dogs', (req,res) => {
+  res.send('ok');
+})
+
+app.get('/test', (req,res) => {
+  res.send('ok');
+})
+
+
+app.get('/aaaa', (req,res) => {
+  res.send('ok');
+})
+
 module.exports = app;
